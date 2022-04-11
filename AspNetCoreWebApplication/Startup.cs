ad3a4cd87df52ae5e0705800595cfcad7ddf19f5
Peter Ksiazek <peter.ksiazek@icloud.com>
2022-04-11 13:10:59 +1000

    Updated package references

--- AspNetCoreWebApplication/Startup.cs
+++ AspNetCoreWebApplication/Startup.cs
@@ -30,7 +30,7 @@ namespace AspNetCoreWebApplication
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
             // Add framework services.
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
