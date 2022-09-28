50e5ed8394fa005885d8bd7f5aa9d29cb7614c2d
Marcos Monteiro <mrcsmonteiro@gmail.com>
2022-09-28 17:13:21 +1000

    Update java-11-openjdk-devel

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -14,6 +14,6 @@ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key 
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install -y jenkins
-sudo apt install -y openjdk-8-jdk
+sudo apt install -y java-11-openjdk-devel
 sudo touch /var/lib/jenkins/jenkins.install.InstallUtil.lastExecVersion
 sudo systemctl restart jenkins
