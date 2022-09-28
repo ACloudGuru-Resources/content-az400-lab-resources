70ae312460304c173ee198f80a8ed371fc8369c9
Marcos Monteiro <mrcsmonteiro@gmail.com>
2022-09-28 19:36:19 +1000

    Update commands

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,19 +1,24 @@
-#!/bin/bash
+#!/bin/bash -xe
+
+# update
+sudo apt-get update -y
 
 # zip
-sudo apt install -y zip
+sudo apt-get install -y zip
 
 # nodejs and npm
-sudo apt install -y nodejs
-sudo apt install -y npm
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
 
 sleep 60
 
+# Java
+sudo apt-get install -y openjdk-11-jre-headless
+
 # Jenkins
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update -y
 sudo apt-get install -y jenkins
-sudo apt install -y java-11-openjdk-devel
 sudo touch /var/lib/jenkins/jenkins.install.InstallUtil.lastExecVersion
 sudo systemctl restart jenkins
